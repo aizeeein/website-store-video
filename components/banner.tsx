fdd661c037387a61ec2cf779a376032b95fd63a5
unknown <zein.abdullah12@gmail.com>
2024-06-06 00:06:26 +0700

    update banner data

--- components/banner.tsx
+++ components/banner.tsx
@@ -13,7 +13,7 @@ const Banner: React.FC<BannerProps> = ({ data }) => {
       >
         <div className="h-full w-full flex flex-col justify-center items-center text-center gap-y-8">
           <div className="font-bold text-3xl sm:text-5xl lg:text-6xl sm:max-w-xl max-w-xs">
-            {data.label}
+            {data?.label}
           </div>
         </div>
       </div>
