e1d7d39e77d6c823a070d9ab15640ace03b44de0
Abdullah Zein <84499267+aizeeein@users.noreply.github.com>
2024-06-08 15:15:40 +0700

    Update gallery-tab.tsx

--- components/gallery/gallery-tab.tsx
+++ components/gallery/gallery-tab.tsx
@@ -14,7 +14,7 @@ const GalleryTab: React.FC<GalleryTabProps> = ({ image }) => {
       {({ selected }) => (
         <div>
           <span className="absolute h-full w-full aspect-square inset-0 overflow-hidden rounded-md">
-            <Image fill src={image.url} alt="" className="object-cocer" />
+            <Image fill src={image.url} alt="" className="object-cover" />
           </span>
           <span
             className={cn(
